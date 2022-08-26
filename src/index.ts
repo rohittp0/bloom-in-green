c61874f2c8a7fb9cde821a5f16b6f165761bae26
Varsha <varshashaheen2003@gmail.com>
2022-08-26 21:11:52 +0530

    slide done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,48 @@
-// Place holder
-export const a = 0;
+import Splide from "@splidejs/splide";
+
+const newsList = document.getElementById("newsList") as HTMLUListElement;
+
+function createSlides(image, title, content) {
+    return `
+        <div class="carousal-content">
+            <div class="news-card">
+                <img src="${image}" alt="">
+                <h4>${title}</h4>
+                <a href="#">
+                    <button>The Hindu</button>
+                </a>
+                <p>${content}</p>
+
+            </div>
+        </div>
+    `;
+}
+
+function createRows() {
+    const colPerRow = 3;
+
+    let cols = [];
+    const rows = []
+
+    for(let i=0; i<10; i++)
+    {
+        cols.push(createSlides("/img/card-img.webp", "The news title", "News content"));
+
+        if(i % colPerRow === 0)
+        {
+            rows.push(`<li class="splide__slide carousal-slide">${cols.join("\n")}</li>`);
+            cols = [];
+        }
+    }
+
+    return rows.reverse().join("\n");
+}
+
+newsList.innerHTML = createRows();
+
+new Splide('.splide', {classes: {
+    arrow: "splide__arrow news-arrow"
+    }}).mount();
 
 console.log(`%cDeveloped by
     Rohit  : https://www.linkedin.com/in/rohit-tp
