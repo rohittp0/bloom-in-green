a2f5fa5633e113a8cbcf623f2d03b60ba4e6e27b
rohittp0 <45072928+rohittp0@users.noreply.github.com>
2022-08-26 18:17:06 +0000

    Build: Merge pull request #4 from rohittp0/rohit
    slides

--- bundleindex.f597ff38a855d9d0ea0a.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("%cDeveloped by\n    Rohit  : https://www.linkedin.com/in/rohit-tp\n    Varsha : https://www.linkedin.com/in/varsha-shaheen-a10295231","background-color:black;border-left: solid red 8px;color:white;padding:10px;border-radius:5px")})();
