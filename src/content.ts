1c0302623b60fc49e123f56579966bbb2e40254c
Rohit T P <tprohit9@gmail.com>
2022-08-30 23:20:45 +0530

    only gallery remaining

---
+++ src/content.ts
@@ -0,0 +1,9 @@
+export const circleData = {
+    "Education": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Music": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Sustainablity": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Spirituality": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Movement": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Health": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Art": {img: "/img/placeholder-icon.png", text: "Education is abc"}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Splide from "@splidejs/splide";
+import {circleData} from "./content";
 
 const newsList = document.getElementById("newsList") as HTMLUListElement;
 const eventsList = document.getElementById("eventsList") as HTMLUListElement;
+const circlesList = document.getElementById("circlesList") as HTMLDivElement;
 
 function createSlides(image, title, content) {
     return `
@@ -43,18 +45,31 @@ function createEvent(image, title, description)
 {
     return `
     <li class="splide__slide">
-                    <div class="event-card">
-                        <img src="${image}" alt="card image" class="event-image">
-                        <div class="hover-panel">
-                            <h4>${title}</h4>
-                            <p>
-                                ${description}
-                            </p>
-                        </div>
-                    </div>
-                </li>`;
+        <div class="event-card">
+            <img src="${image}" alt="card image" class="event-image">
+            <div class="hover-panel">
+                <h4>${title}</h4>
+                <p>${description}</p>
+            </div>
+        </div>
+    </li>`;
 }
 
+function createCircles() {
+    const elements = [];
+
+    for(const title in circleData)
+        elements.push(`
+    <img src="${circleData[title].img}" alt="" tabindex="1">
+    <div class="circle-center">
+        <h6>${title}</h6>
+        <p>${circleData[title].text}</p>
+    </div>`);
+
+    return elements.join("\n");
+}
+
+circlesList.innerHTML = createCircles();
 newsList.innerHTML = createRows();
 eventsList.innerHTML = [...Array(10).keys()]
     .map(() => createEvent("/img/festival-card.webp", "Title", "Details"))
