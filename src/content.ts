8352bab942055dd8e40def9b16cada1a1f59c801
Rohit T P <tprohit9@gmail.com>
2022-09-05 02:25:22 +0530

    bug fix

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,7 @@
 export const circleData = {
-    "Education": {img: "/img/placeholder-icon.png", text: "Education is abc"},
-    "Music": {img: "/img/placeholder-icon.png", text: "Education is abc"},
-    "Sustainablity": {img: "/img/placeholder-icon.png", text: "Education is abc"},
+    "Education": {img: "/img/placeholder-icon.png", text: "Education is abc Education is abc Education is abc "},
+    "Music": {img: "/img/placeholder-icon.png", text: "Education is abc Education is abc Education is abc "},
+    "Sustainablity": {img: "/img/placeholder-icon.png", text: "Education is abc Education is abc Education is abc "},
     "Spirituality": {img: "/img/placeholder-icon.png", text: "Education is abc"},
     "Movement": {img: "/img/placeholder-icon.png", text: "Education is abc"},
     "Health": {img: "/img/placeholder-icon.png", text: "Education is abc"},
