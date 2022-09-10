f8cdcc50d5cf4bdd8053aff32cf10f4c9d14ca6b
Rohit T P <tprohit9@gmail.com>
2022-09-10 15:18:56 +0530

    title and description added

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -12,5 +12,15 @@ export const eventDetails = [
     ["Easy Wanderlings", "Live"],
     ["Fire Hoop", "Performance Art"],
     ["Hang in balance ft Flavio Lopez", "Live"],
+    ["Shanka Tribe", "Live"],
+    ["Art of Love", "Workshop"],
+    ["Hoop & Poi" , "Workshop"],
+    ["Main Stage", ""],
+    ["Drum Circle", "Community session"],
+    ["The F16s" , "Live"],
+    ["Sound Healing", "Session"],
+    ["Ecstatic Dance", "Session"],
+    ["Stories Worth Sharing", "Session"],
+    ["Contact Improvisation.", "Session"]
 ]
 
