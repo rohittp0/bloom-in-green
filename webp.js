0057ae0052214e273430beec6590d0aeb5b35307
Rohit T P <tprohit9@gmail.com>
2022-09-17 16:02:58 +0530

    bug fix

--- webp.js
+++ webp.js
@@ -22,15 +22,15 @@ const noConvert = ["webp", "m4v", "mp4", "mkv"];
 const images = getFiles(path.resolve("public/img")).filter((img) => noConvert.indexOf(img.split(".")[1]) === -1);
 const html = getFiles(path.resolve("src/templates")).filter((html) => html.endsWith(".html"));
 
-// images.forEach((img) => webp.cwebp(img, img.split(".")[0] + ".webp"));
+images.forEach((img) => webp.cwebp(img, img.split(".")[0] + ".webp"));
 
 html.forEach(async (p) =>{
-    const content = fs.readFileSync(p, 'utf8');
+    let content = fs.readFileSync(p, 'utf8');
 
     images.forEach((img) =>
     {
         img = img.replace(base+"\\", "/").replaceAll("\\\\", "/");
-        content.replaceAll(img, img.split(".")[0] + ".webp");
+        content = content.replaceAll(img, img.split(".")[0] + ".webp");
     });
 
     fs.writeFile(p, content, 'utf8', console.log);
