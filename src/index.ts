046d3d75ef90eb5fe2b44a68b2f02794e220f110
Rohit T P <tprohit9@gmail.com>
2022-09-26 18:19:49 +0530

    trimmed

--- src/index.ts
+++ src/index.ts
@@ -3,46 +3,10 @@ import Splide from "@splidejs/splide";
 
 import {eventDetails, srcsetSizes} from "./utils/content";
 
-// const newsList = document.getElementById("newsList") as HTMLUListElement;
 const eventsList = document.getElementById("eventsList") as HTMLUListElement;
-// const gallery = document.getElementById("gallery") as HTMLDivElement;
 const video = document.getElementById("heroMovie") as HTMLIFrameElement;
 const close = document.getElementById("closeBanner") as HTMLButtonElement;
 const banner = document.getElementById("banner") as HTMLDivElement;
-
-// function createSlides(image, title, content) {
-//     return `
-//         <div class="carousal-content">
-//             <div class="news-card">
-//                 <img loading="lazy" src="${image}" alt="">
-//                 <h4>${title}</h4>
-//                 <a href="#">
-//                     <button>The Hindu</button>
-//                 </a>
-//                 <p>${content}</p>
-//
-//             </div>
-//         </div>
-//     `;
-// }
-
-// function createRows() {
-//     const colPerRow = 3;
-//
-//     let cols = [];
-//     const rows = []
-//
-//     for (let i = 0; i < 10; i++) {
-//         cols.push(createSlides("/img/index/news-card.webp", "The news title", "News content"));
-//
-//         if (i % colPerRow === 0) {
-//             rows.push(`<li class="splide__slide carousal-slide">${cols.join("\n")}</li>`);
-//             cols = [];
-//         }
-//     }
-//
-//     return rows.reverse().join("\n");
-// }
 
 function createEvent(image, title, description) {
     const parts = image.split("/");
@@ -63,38 +27,11 @@ function createEvent(image, title, description) {
     </li>`;
 }
 
-// function getPercentOfView(element) {
-//     const viewTop = window.pageYOffset;
-//     const viewBottom = viewTop + window.innerHeight;
-//     const rect = element.getBoundingClientRect();
-//     const elementTop = rect.top + viewTop;
-//     const elementBottom = elementTop + rect.height;
-//
-//     if (elementBottom <= viewTop)
-//         return -1;
-//     else if (elementTop >= viewBottom)
-//         return 0.3;
-//
-//     else if (elementBottom <= viewBottom) {
-//         if (elementTop < viewTop || true)
-//             return (elementBottom - viewTop) / window.innerHeight ;
-//         else
-//             return (elementBottom - elementTop) / window.innerHeight ;
-//     } else
-//         return (viewBottom - elementTop) / window.innerHeight;
-// }
-
-// newsList.innerHTML = createRows();
-
 eventsList.innerHTML = eventDetails
-    .map((v, i) => createEvent(`/img/index/event/(${i+1}).webp`, v[0], v[1]))
+    .map((v, i) =>
+        createEvent(`https://rohittp.imgix.net/img/index/event/(${i+1}).webp`, v[0], v[1]))
     .join("\n");
 
-// new Splide(".news-slide", {
-//     classes: {
-//         arrow: "splide__arrow news-arrow"
-//     }
-// }).mount();
 
 new Splide(".event-slide", {
     classes: {
@@ -110,14 +47,6 @@ new Splide(".community-slide", {
     autoWidth: false
 }).mount();
 
-// window.addEventListener("scroll", () =>
-//     document.body.style.setProperty("--scroll",
-//         String(window.pageYOffset / (gallery.offsetTop - gallery.offsetHeight))), {passive: true});
-//
-// window.addEventListener("scroll", () =>{
-//     const scroll = String(getPercentOfView(gallery));
-//     document.body.style.setProperty("--gallery-scroll", scroll);
-// }, {passive: true});
 
 document.addEventListener('fullscreenchange', onFullScreenChange, {passive: true});
 document.addEventListener('webkitfullscreenchange', onFullScreenChange, {passive: true});
@@ -146,6 +75,8 @@ document.querySelectorAll("video").forEach((v) => v.play());
 
 if(window.innerWidth > 414)
     (document.getElementById("videoDesktop") as HTMLVideoElement).src = "/img/index/web-banner.m4v";
+else
+    (document.getElementById("videoMobile") as HTMLVideoElement).src = "/img/index/web-banner-mobile.m4v";
 
 console.log(`%cDeveloped by
     Rohit  : https://www.linkedin.com/in/rohit-tp
