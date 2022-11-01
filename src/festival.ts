33d9588ad52bf8bddc22e77620727e8296b4250e
Rohit T P <tprohit9@gmail.com>
2022-11-01 21:42:26 +0530

    all changes

--- src/festival.ts
+++ src/festival.ts
@@ -3,8 +3,7 @@ import {lineUP} from "./utils/content";
 const div = document.getElementById("lineup");
 const root = lineUP.root
 
-function getSrcset(path)
-{
+function getSrcset(path) {
     const sizes = [415, 375, 390, 300, 260, 230];
 
     return sizes.map((w) => `${path}?w=${w}&fit=max ${w}w`).join(", ");
@@ -15,7 +14,7 @@ function generateRow(root, names, key) {
     const title = key.toUpperCase().replaceAll("_", " ")
     const images = names.map((name, i) => `
         <div class="card-image">
-            <img src="${root}/${key}/${i+1}.webp" srcset="${getSrcset(`${root}/${key}/${i+1}.webp`)}" 
+            <img src="${root}/${key}/${i + 1}.webp" srcset="${getSrcset(`${root}/${key}/${i + 1}.webp`)}" 
                 alt="${name}">
             <p>${name}</p>
         </div>
@@ -31,7 +30,30 @@ function generateRow(root, names, key) {
     `
 }
 
-const rows = Object.keys(lineUP.categories).map((key) =>
+const rowsHtml = Object.keys(lineUP.categories).map((key) =>
     generateRow(root, lineUP.categories[key], key));
 
-div.innerHTML = div.innerHTML.replace("<span data-id=\"image-grid\"></span>", rows.join(""))
+div.innerHTML = div.innerHTML.replace("<span data-id=\"image-grid\"></span>", rowsHtml.join(""));
+
+const scrollStep = 10, slideTime = 2000;
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+document.querySelectorAll('.row').forEach((row) => {
+    const inter = setInterval(async () => {
+        if (row.scrollLeft >= row.scrollWidth)
+            return clearInterval(inter);
+
+        const max = row.scrollLeft + (window.innerWidth > 415 ? 250 : window.innerWidth);
+
+        for (let i = row.scrollLeft; i <= max+1; i+= scrollStep) {
+            console.log(i)
+            row.scrollTo(i, 0);
+            await sleep(slideTime / max);
+        }
+
+        row.scrollTo(max, 0);
+
+    }, slideTime);
+
+    row.addEventListener("click", () => clearInterval(inter));
+});
